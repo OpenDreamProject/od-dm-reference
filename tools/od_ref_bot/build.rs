702be4181c24e9191fb502ce361b6e17bfacf756
harryob <55142896+harryob@users.noreply.github.com>
2024-11-12 20:53:37 +0000

    improves the build script with `SOURCE_DIR` env var, adds Dockerfile and example docker-compose.yml

--- tools/od_ref_bot/build.rs
+++ tools/od_ref_bot/build.rs
@@ -9,6 +9,8 @@ const SOURCE_DIR: &str = "../../content";
 
 fn main() -> Result<(), Box<dyn Error>> {
     let dest_path = Path::new("./src/content.rs");
+    let from_path = std::env::var("SOURCE_DIR").unwrap_or(SOURCE_DIR.to_string());
+
     let mut out_files = File::create(dest_path)?;
 
     writeln!(
@@ -16,14 +18,14 @@ fn main() -> Result<(), Box<dyn Error>> {
         r##"use std::collections::HashMap;pub fn get_all() -> HashMap<&'static str, &'static str> {{ let mut out = HashMap::new();"##,
     )?;
 
-    let _ = visit_dir(&mut out_files, SOURCE_DIR);
+    let _ = visit_dir(&mut out_files, from_path.as_str(), &from_path);
 
     writeln!(&mut out_files, r##"out}}"##,)?;
 
     Ok(())
 }
 
-fn visit_dir(file: &mut File, dir: &str) -> Result<(), Box<dyn Error>> {
+fn visit_dir(file: &mut File, dir: &str, from_dir: &String) -> Result<(), Box<dyn Error>> {
     for inner_file in fs::read_dir(dir)? {
         let inner_file = inner_file?;
 
@@ -31,7 +33,7 @@ fn visit_dir(file: &mut File, dir: &str) -> Result<(), Box<dyn Error>> {
 
         if !file_type.is_file() {
             if file_type.is_dir() {
-                let _ = visit_dir(file, inner_file.path().to_str().unwrap());
+                let _ = visit_dir(file, inner_file.path().to_str().unwrap(), from_dir);
             }
 
             continue;
@@ -42,7 +44,7 @@ fn visit_dir(file: &mut File, dir: &str) -> Result<(), Box<dyn Error>> {
             r##"out.insert("{name}", include_str!(r#"{path}"#));"##,
             name = inner_file
                 .path()
-                .strip_prefix(SOURCE_DIR)
+                .strip_prefix(from_dir)
                 .unwrap()
                 .to_string_lossy()
                 .replace("\\", "/"),
@@ -52,3 +54,4 @@ fn visit_dir(file: &mut File, dir: &str) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+    
