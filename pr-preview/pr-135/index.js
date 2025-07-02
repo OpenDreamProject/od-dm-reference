190a0218313204c6bfa0b31db1fa8035b8ee0ea9
hry-gh <hry-gh@users.noreply.github.com>
2025-07-02 08:07:16 +0000

    Deploy preview for PR 135 🛫

---
+++ pr-preview/pr-135/index.js
@@ -0,0 +1,45 @@
+const handleClick = () => {
+    const element = document.getElementById("left-bar");
+    if(element.style.display == "block") {
+        element.classList.remove("animate-in");
+        element.classList.add("animate-out");
+        setTimeout(() => {element.style.display = "none"}, 250)
+    } else {
+        element.style.display = "block";
+        element.classList.remove("animate-out");
+        element.classList.add("animate-in");
+    }
+    
+}
+
+const handleNavClick = (event) => {
+    event.preventDefault()
+    event.target.classList.toggle("fa-rotate-90")
+    const items = event.target.parentNode.parentNode.parentNode.children
+    for(const item of items) {
+        if(item.localName == "ul") {
+            for(const child of item.children) {
+                child.classList.toggle("hidden")
+            }
+        }
+    }
+}
+
+const handleThemeSwitch = (event) => {
+    const newTheme = document.documentElement.classList.contains(THEME_CLASS_DARK) ? THEME_CLASS_LIGHT : THEME_CLASS_DARK
+    localStorage.setItem(LOCAL_STORAGE_KEY_THEME, newTheme)
+    setTheme()
+}
+
+const main = () => {
+    document.getElementById("navigation-button").addEventListener("click", handleClick)
+
+    const toggles = document.getElementsByClassName("nav-toggle")
+    for(const item of toggles) {
+        item.addEventListener("click", handleNavClick);
+    }
+
+    document.getElementById("theme-switch").addEventListener("click", handleThemeSwitch)
+}
+
+window.onload = main;
